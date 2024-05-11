2bc61717e3c47c295d1c5cc4a0f869b588fec217
kelldani <87382954+khalildahbane@users.noreply.github.com>
2024-05-11 14:49:33 +0100

    Markdown to HTML

---
+++ markdown2html.py
@@ -0,0 +1,34 @@
+#!/usr/bin/python3
+'''A script that codes markdown to HTML'''
+
+import sys
+import re
+import os
+
+ # Test that the number of arguments passed is 2
+    if len(sys.argv[1:]) != 2:
+        print('Usage: ./markdown2html.py README.md README.html',
+              file=sys.stderr)
+        sys.exit(1)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+if __name__ == '__main__':
