97b2b6faa72ef6e14537f15a8134f2ead1f0aaf6
kelldani <87382954+khalildahbane@users.noreply.github.com>
2024-05-11 14:51:31 +0100

    Markdown to HTML

--- markdown2html.py
+++ markdown2html.py
@@ -11,24 +11,32 @@ import os
               file=sys.stderr)
         sys.exit(1)
 
+  # Store the arguments into variables
+    input_file = sys.argv[1]
+    output_file = sys.argv[2]
 
+    # Checks that the markdown file exists and is a file
+    if not (os.path.exists(input_file) and os.path.isfile(input_file)):
+        print(f'Missing {input_file}', file=sys.stderr)
+        sys.exit(1)
 
+    with open(input_file, encoding='utf-8') as file_1:
+        html_content = []
+        md_content = [line[:-1] for line in file_1.readlines()]
+        for line in md_content:
+            heading = re.split(r'#{1,6} ', line)
+            if len(heading) > 1:
+                # Compute the number of the # present to
+                # determine heading level
+                h_level = len(line[:line.find(heading[1])-1])
+                # Append the html equivalent of the heading
+                html_content.append(
+                    f'<h{h_level}>{heading[1]}</h{h_level}>\n'
+                )
+            else:
+                html_content.append(line)
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+    with open(output_file, 'w', encoding='utf-8') as file_2:
+        file_2.writelines(html_content)
 
 if __name__ == '__main__':
